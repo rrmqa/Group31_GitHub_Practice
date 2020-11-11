e6a2964a9303ee905f54edb9fd2ced4c2788f4cc
CihanSaracoglu <csarac3506@gmail.com>
2020-11-11 19:52:13 -0200

    Added comment to the featureA

--- src/main/java/Group31_Main.java
+++ src/main/java/Group31_Main.java
@@ -1,5 +1,7 @@
 public class Group31_Main {
     public static void main(String[] args) {
         System.out.println("We have started at main class");
+
+        // this is a commit from FeatureA
     }
 }
