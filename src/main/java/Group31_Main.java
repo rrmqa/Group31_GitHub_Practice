5159b7579b206e988e5837c0d0d5bde98974829e
CihanSaracoglu <csarac3506@gmail.com>
2020-11-11 19:50:32 -0200

    Added main class to the Group31Main

---
+++ src/main/java/Group31_Main.java
@@ -0,0 +1,5 @@
+public class Group31_Main {
+    public static void main(String[] args) {
+        System.out.println("We have started at main class");
+    }
+}
---
+++ src/test/java/Group31_Test.java
@@ -0,0 +1,5 @@
+public class Group31_Test {
+    public static void main(String[] args) {
+        System.out.println("We have just started");
+    }
+}
